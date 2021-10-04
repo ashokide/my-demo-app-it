be9dc6e96d2ecd51c0622b10607c37bb67f5b371
Ashok N <66304695+ashokide@users.noreply.github.com>
2021-10-04 12:14:49 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-    res.json('Hey This Is working');
+    res.json('Hey This Is working,Running on Port: '+PORT);
 })
 
-const PORT = process.env.PORT || 3000;
+
 app.listen(PORT,()=>{
     console.log('listening on port: '+PORT+'🚀');
 });
